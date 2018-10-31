0db6976ab675cad9fbb13ec55e676328d9b6d195
TSANG <wai@tsangfamily.org>
2018-10-31 16:57:38 +0000

    Initial commit

---
+++ app/src/main/java/com/example/tsang/listfrag/ui/main/MainFragment.java
@@ -0,0 +1,39 @@
+package com.example.tsang.listfrag.ui.main;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.tsang.listfrag.R;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    private RecyclerView myRecyclerView;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+        myRecyclerView = (RecyclerView) this.getActivity().findViewById(R.id.myRecyclerView);
+    }
+}
